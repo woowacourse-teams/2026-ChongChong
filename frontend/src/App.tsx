547092e2fbea898343368ef00f3d6a1d22ac1323
SiHyunLee <antoliny0919@gmail.com>
2026-08-16 17:02:10 +0900

    [feat] 공통 다이얼로그, 헤더, 뱃지 컴포넌트 추가 (#33)
    * [feat]: 공통 다이얼로그 컴포넌트 추가
    
    * [feat]: 공통 탑헤더 컴포넌트 추가
    
    * [feat]: 공통 뱃지 컴포넌트 추가
    
    * [fix]: badge에 rest 프랍 추가
    
    * [fix]: dialog가 접근성을 준수하도록 수정
    
    * [fix]: 오타 수정
    
    * [fix]: 파스칼 케이스를 카멜 케이스로 변경
    
    * [fix]: 타입 단언을 satisfies 로 전환
    
    * [refactor]: 불필요한 스프레들 연산 제거
    
    * [refactor]: rest 파라미터를 props로 일관성을 위해 전환
    
    * [refactor]: 불필요한 타입 단언 전환

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,3 @@
-import Button from './shared/ui/Button';
-
 export default function App() {
-  return (
-    <div>
-      <Button variant="CriticalOutline" size="Large">
-        hello world
-      </Button>
-    </div>
-  );
+  return <div></div>;
 }
