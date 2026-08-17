d017d8f6711fcb20468ab308137e2239934ddaef
wontop02 <wjy2002kr@gmail.com>
2026-08-17 22:15:26 +0900

    chore: ChongChongApplicationTests에 테스트 프로파일 추가

--- backend/src/test/java/withoutc/chongchong/ChongChongApplicationTests.java
+++ backend/src/test/java/withoutc/chongchong/ChongChongApplicationTests.java
@@ -2,8 +2,10 @@ package withoutc.chongchong;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class ChongChongApplicationTests {
 
     @Test
