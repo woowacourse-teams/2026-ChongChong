b74f8df91fda11315c40982f3289360d17ccae11
wontop02 <wjy2002kr@gmail.com>
2026-08-17 21:15:45 +0900

    chore: GlobalExceptionHandlerTest 컨텍스트 격리 설정

--- backend/src/test/java/withoutc/chongchong/global/exception/GlobalExceptionHandlerTest.java
+++ backend/src/test/java/withoutc/chongchong/global/exception/GlobalExceptionHandlerTest.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@WebMvcTest
+@WebMvcTest(controllers = GlobalExceptionHandlerTest.TestController.class)
 @Import({GlobalExceptionHandler.class, GlobalExceptionHandlerTest.TestController.class})
 class GlobalExceptionHandlerTest {
 
