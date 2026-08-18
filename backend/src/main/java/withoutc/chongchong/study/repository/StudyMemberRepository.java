d4155529c98c7d72f7e5b88e21d1479261c6a6e0
frombunny <rucy011121@gmail.com>
2026-08-18 20:44:46 +0900

    feat: Study, StudyMember 조회 및 예외 처리 추가

--- backend/src/main/java/withoutc/chongchong/study/repository/StudyMemberRepository.java
+++ backend/src/main/java/withoutc/chongchong/study/repository/StudyMemberRepository.java
@@ -3,7 +3,12 @@ package withoutc.chongchong.study.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import withoutc.chongchong.study.entity.StudyMember;
+import withoutc.chongchong.study.exception.StudyMemberErrorCode;
+import withoutc.chongchong.study.exception.StudyMemberException;
 
 @Repository
 public interface StudyMemberRepository extends JpaRepository<StudyMember, Long> {
+    default StudyMember findByIdOrThrow(Long id) {
+        return findById(id).orElseThrow(() -> new StudyMemberException(StudyMemberErrorCode.STUDY_MEMBER_NOT_FOUND));
+    }
 }
---
+++ backend/src/main/java/withoutc/chongchong/study/repository/StudyRepository.java
@@ -0,0 +1,14 @@
+package withoutc.chongchong.study.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import withoutc.chongchong.study.entity.Study;
+import withoutc.chongchong.study.exception.StudyErrorCode;
+import withoutc.chongchong.study.exception.StudyException;
+
+@Repository
+public interface StudyRepository extends JpaRepository<Study, Long> {
+    default Study findByIdOrThrow(Long id) {
+        return findById(id).orElseThrow(() -> new StudyException(StudyErrorCode.STUDY_NOT_FOUND));
+    }
+}
