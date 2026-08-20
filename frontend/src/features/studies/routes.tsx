a3ebdbb54e58918506aa056570664118a1ec0954
Antoliny0919 <antoliny0919@gmail.com>
2026-08-21 00:06:48 +0900

    [feat]: 스터디 디테일 페이지 라우트 추가

--- frontend/src/features/studies/routes.tsx
+++ frontend/src/features/studies/routes.tsx
@@ -1,6 +1,7 @@
 import { RouteObject } from 'react-router';
 import MyStudiesPage from './pages/MyStudiesPage';
 import NewStudyPage from './pages/NewStudyPage';
+import StudyDetailPage from './pages/StudyDetailPage';
 
 export const routes: RouteObject[] = [
   {
@@ -10,5 +11,9 @@ export const routes: RouteObject[] = [
   {
     path: '/studies/new',
     element: <NewStudyPage />,
+  },
+  {
+    path: '/studies/:studyId',
+    element: <StudyDetailPage />,
   },
 ];
