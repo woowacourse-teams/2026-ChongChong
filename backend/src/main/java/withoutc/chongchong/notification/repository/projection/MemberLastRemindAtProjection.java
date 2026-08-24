e96921d2cdc127bedd4f7b47ccf0011c2b313aec
frombunny <rucy011121@gmail.com>
2026-08-24 14:28:33 +0900

    chore: 사용하지 않는 파일 삭제

--- backend/src/main/java/withoutc/chongchong/notification/repository/projection/MemberLastRemindAtProjection.java
+++
@@ -1,9 +0,0 @@
-package withoutc.chongchong.notification.repository.projection;
-
-import java.time.LocalDateTime;
-
-public record MemberLastRemindAtProjection(
-        Long memberId,
-        LocalDateTime lastRemindAt
-) {
-}
