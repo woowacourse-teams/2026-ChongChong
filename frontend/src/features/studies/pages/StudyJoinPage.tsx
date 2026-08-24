98bb90cfac57a4178e41aba3fbd450b8a1b0435b
Antoliny0919 <antoliny0919@gmail.com>
2026-08-24 19:22:48 +0900

    [feat]: 스터디 참여 페이지 UI 추가

---
+++ frontend/src/features/studies/pages/StudyJoinPage.tsx
@@ -0,0 +1,39 @@
+import Main from '../../../shared/ui/Main';
+import Page from '../../../shared/ui/Page';
+import TopHeader from '../../../shared/ui/TopHeader';
+import { PrevButton } from '../../../shared/ui/components/PrevButton';
+import Field from '../../../shared/ui/inputs/Field';
+import Input from '../../../shared/ui/inputs/Input';
+import { useInputState } from '../../../shared/hooks/useInputState';
+import Button from '../../../shared/ui/Button';
+import { tokens } from '../../../styles/global';
+
+export default function StudyJoinPage() {
+  const [joinLink, handleJoinLink] = useInputState();
+
+  return (
+    <Page>
+      <TopHeader
+        left={<PrevButton />}
+        middle={<TopHeader.Title>스터디 참여하기</TopHeader.Title>}
+      />
+      <Main>
+        <form css={{ margin: `${tokens.spacing[5]} 0` }}>
+          <Field
+            id="study-name"
+            isRequired={true}
+            label="초대 링크"
+            helpText="스터디 리드에게 받은 초대 링크를 붙여넣어 주세요"
+            // errorText={errors.name.message}
+            // isError={errors.name.state}
+          >
+            <Input id="study-name" value={joinLink} onChange={handleJoinLink} maxLength={15} />
+          </Field>
+          <Button css={{ marginTop: tokens.spacing[5] }} variant="brandSolid" size="large">
+            스터디 참여하기
+          </Button>
+        </form>
+      </Main>
+    </Page>
+  );
+}
--- frontend/src/features/studies/routes.tsx
+++ frontend/src/features/studies/routes.tsx
@@ -2,6 +2,7 @@ import { RouteObject } from 'react-router';
 import MyStudiesPage from './pages/MyStudiesPage';
 import CreateStudyPage from './pages/CreateStudyPage';
 import StudyDetailPage from './pages/StudyDetailPage';
+import StudyJoinPage from './pages/StudyJoinPage';
 
 export const routes: RouteObject[] = [
   {
@@ -15,5 +16,9 @@ export const routes: RouteObject[] = [
   {
     path: '/studies/:studyId',
     element: <StudyDetailPage />,
+  },
+  {
+    path: '/studies/join',
+    element: <StudyJoinPage />,
   },
 ];
