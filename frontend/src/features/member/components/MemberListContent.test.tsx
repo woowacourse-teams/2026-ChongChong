555bb610cf88d443323bbcb3a25801c5e2629664
Antoliny0919 <antoliny0919@gmail.com>
2026-08-25 01:09:38 +0900

    [chore] 불필요한 개행 제거

--- frontend/src/features/member/components/MemberListContent.test.tsx
+++ frontend/src/features/member/components/MemberListContent.test.tsx
@@ -22,20 +22,15 @@ describe('초대 링크 테스트', () => {
   test('버튼을 클릭하면 초대 링크가 복사된다', async () => {
     mockStudyInviteLink();
     const user = userEvent.setup();
-
     const writeText = jest.spyOn(navigator.clipboard, 'writeText').mockResolvedValue();
-
     render(<InviteLinkBox inviteLink={'mock-chongchong-invite-link123'} />);
-
     const copyButton = await screen.findByRole('button', {
       name: '링크 복사',
     });
-
     await user.click(copyButton);
 
     // 이후에 API 값으로 전환
     expect(writeText).toHaveBeenCalledWith('mock-chongchong-invite-link123');
-
     writeText.mockRestore();
   });
 });
