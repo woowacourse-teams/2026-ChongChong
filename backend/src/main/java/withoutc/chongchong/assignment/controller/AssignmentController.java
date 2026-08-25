7bad2f9988e288271486c2e5b8a38438d3202faa
frombunny <rucy011121@gmail.com>
2026-08-25 09:55:00 +0900

    feat(assignment): 과제 수정 API 작성

--- backend/src/main/java/withoutc/chongchong/assignment/controller/AssignmentController.java
+++ backend/src/main/java/withoutc/chongchong/assignment/controller/AssignmentController.java
@@ -6,6 +6,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import withoutc.chongchong.assignment.controller.dto.AssignmentCreateRequest;
 import withoutc.chongchong.assignment.controller.dto.AssignmentCreateResponse;
+import withoutc.chongchong.assignment.controller.dto.AssignmentUpdateRequest;
 import withoutc.chongchong.assignment.service.AssignmentService;
 import withoutc.chongchong.auth.security.AuthenticatedUser;
 
@@ -30,6 +32,16 @@ public class AssignmentController {
         AssignmentCreateResponse response = assignmentService.create(currentUser.id(), studyId, request);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
+    }
+
+    @PatchMapping("{assignmentId}")
+    public ResponseEntity<Void> updateAssignment(@AuthenticationPrincipal AuthenticatedUser currentUser,
+                                                 @PathVariable Long studyId, @PathVariable Long assignmentId,
+                                                 @Valid AssignmentUpdateRequest request
+    ) {
+        assignmentService.update(currentUser.id(), studyId, assignmentId, request);
+
+        return ResponseEntity.noContent().build();
     }
 
     @DeleteMapping("{assignmentId}")
