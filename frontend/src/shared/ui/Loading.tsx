70dc3a9c9f39f0739a2881d78ce86dbba5c5a4d3
eunnn <ehddms0517@naver.com>
2026-08-27 00:23:28 +0900

    feat: 로딩 컴포넌트 추가

---
+++ frontend/src/shared/ui/Loading.tsx
@@ -0,0 +1,19 @@
+import { CSSProperties } from 'react';
+import loadingImage from '../assets/icons/loading-running-rabbit.webp';
+
+const divStyle = {
+  display: 'flex',
+  width: '100%',
+  minHeight: '100dvh',
+  flexDirection: 'column',
+  justifyContent: 'center',
+  alignItems: 'center',
+} satisfies CSSProperties;
+
+export default function Loading() {
+  return (
+    <div css={divStyle}>
+      <img src={loadingImage} alt="로딩 중" width={150} height={150} />
+    </div>
+  );
+}
