0c6a7e8ae9349d70b0f7bbdf21dd3b5d2ef3fc71
JYL35 <154980427+JYL35@users.noreply.github.com>
2026-08-27 21:41:30 +0900

    feat: 리더의 스터디 멤버 방출 API 구현

--- backend/src/main/java/withoutc/chongchong/study/controller/StudyMemberController.java
+++ backend/src/main/java/withoutc/chongchong/study/controller/StudyMemberController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -44,5 +45,16 @@ public class StudyMemberController {
         StudyMembersResponse response = studyMemberService.getAllStudyMembers(user.id(), studyId);
 
         return ResponseEntity.ok(response);
+    }
+
+    @DeleteMapping("/{studyId}/members/{memberId}")
+    public ResponseEntity<Void> expel(
+            @AuthenticationPrincipal AuthenticatedUser user,
+            @PathVariable @Positive(message = "스터디 ID는 양수여야 합니다.") Long studyId,
+            @PathVariable @Positive(message = "스터디 멤버 ID는 양수여야 합니다.") Long memberId
+    ) {
+        studyMemberService.expel(user.id(), studyId, memberId);
+
+        return ResponseEntity.noContent().build();
     }
 }
