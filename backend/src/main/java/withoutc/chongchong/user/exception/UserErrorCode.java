38179445b7ef3073479831c2c6e58ef1bed53edb
JYL35 <154980427+JYL35@users.noreply.github.com>
2026-09-02 23:08:30 +0900

    refactor: User 생성 불변식 보호

--- backend/src/main/java/withoutc/chongchong/user/exception/UserErrorCode.java
+++ backend/src/main/java/withoutc/chongchong/user/exception/UserErrorCode.java
@@ -9,6 +9,14 @@ import withoutc.chongchong.global.exception.code.ErrorCode;
 @RequiredArgsConstructor
 public enum UserErrorCode implements ErrorCode {
 
+    INVALID_USER_NAME(HttpStatus.BAD_REQUEST, "INVALID_USER_NAME", "사용자 이름이 올바르지 않습니다."),
+
+    INVALID_USER_PROFILE_IMAGE_URL(
+            HttpStatus.BAD_REQUEST,
+            "INVALID_USER_PROFILE_IMAGE_URL",
+            "사용자 프로필 이미지 URL이 올바르지 않습니다."
+    ),
+
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "USER_NOT_FOUND", "해당 사용자를 찾을 수 없습니다."),
     ;
 
