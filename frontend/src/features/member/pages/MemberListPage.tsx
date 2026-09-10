58abf798a4c25c66e8073e8f464bcd52af85d5fe
Antoliny0919 <antoliny0919@gmail.com>
2026-09-10 21:09:13 +0900

    [feat] 멤버 목록에서 네비게이션과 컨텐츠에 에러 경계 추가

--- frontend/src/features/member/pages/MemberListPage.tsx
+++ frontend/src/features/member/pages/MemberListPage.tsx
@@ -1,5 +1,6 @@
 import { Suspense } from 'react';
 import { useSuspenseQuery } from '@tanstack/react-query';
+import { ErrorBoundary, getErrorMessage } from 'react-error-boundary';
 import Main from '../../../shared/ui/Main';
 import Page from '../../../shared/ui/Page';
 import TopHeader from '../../../shared/ui/TopHeader';
@@ -8,23 +9,32 @@ import MemberListContent from '../components/MemberListContent';
 import useIntegerParams from '../../../shared/hooks/useIntegerParams';
 import BottomTab from '../../../shared/widgets/BottomTab';
 import Loading from '../../../shared/ui/Loading';
+import ErrorContent from '../../../shared/ui/ErrorContent';
 import studyQueries from '../../study/queries';
 
 export default function MemberListPage() {
+  return (
+    <Page>
+      <TopHeader left={<PrevButton />} middle={<TopHeader.Title>멤버</TopHeader.Title>} />
+      <Main>
+        <ErrorBoundary
+          fallbackRender={({ error }) => <ErrorContent message={getErrorMessage(error)} />}
+        >
+          <Suspense fallback={<Loading />}>
+            <MemberListPage.Content />
+          </Suspense>
+        </ErrorBoundary>
+      </Main>
+      <BottomTab />
+    </Page>
+  );
+}
+
+MemberListPage.Content = function Content() {
   const { studyId } = useIntegerParams(['studyId']);
   const {
     data: { role },
   } = useSuspenseQuery(studyQueries.info(studyId));
 
-  return (
-    <Page>
-      <Suspense fallback={<Loading />}>
-        <TopHeader left={<PrevButton />} middle={<TopHeader.Title>멤버</TopHeader.Title>} />
-        <Main>
-          {role === 'LEADER' ? <MemberListContent.Leader /> : <MemberListContent.Member />}
-        </Main>
-      </Suspense>
-      <BottomTab />
-    </Page>
-  );
-}
+  return role === 'LEADER' ? <MemberListContent.Leader /> : <MemberListContent.Member />;
+};
