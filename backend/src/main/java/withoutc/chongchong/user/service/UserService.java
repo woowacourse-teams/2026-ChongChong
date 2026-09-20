8c77f3e3959390b042951c7ce16304824adccc7d
JYL35 <154980427+JYL35@users.noreply.github.com>
2026-09-21 00:01:56 +0900

    fix: 탈퇴 시 리더 역할 존재 여부만 조회

--- backend/src/main/java/withoutc/chongchong/user/service/UserService.java
+++ backend/src/main/java/withoutc/chongchong/user/service/UserService.java
@@ -3,14 +3,12 @@ package withoutc.chongchong.user.service;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import withoutc.chongchong.study.entity.StudyMember;
+import withoutc.chongchong.study.entity.StudyMemberRole;
 import withoutc.chongchong.study.repository.StudyMemberRepository;
 import withoutc.chongchong.user.entity.User;
 import withoutc.chongchong.user.exception.UserErrorCode;
 import withoutc.chongchong.user.exception.UserException;
 import withoutc.chongchong.user.repository.UserRepository;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,12 +21,7 @@ public class UserService {
     @Transactional
     public void withdraw(Long userId) {
         User user = userRepository.getByIdForUpdateOrThrow(userId);
-        List<StudyMember> studyMembers = studyMemberRepository.findAllByUserIdOrderByCreatedAtDesc(userId);
-
-        boolean hasLeader = studyMembers.stream()
-                .anyMatch(StudyMember::isLeader);
-
-        if (hasLeader) {
+        if (studyMemberRepository.existsByUserIdAndRole(userId, StudyMemberRole.LEADER)) {
             throw new UserException(UserErrorCode.STUDY_LEADER_WITHDRAWAL_BLOCKED);
         }
 
