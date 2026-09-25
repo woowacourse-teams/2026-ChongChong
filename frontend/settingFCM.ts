e539f72763ef2fa310aa19af5fa2ea3205478be7
Antoliny0919 <antoliny0919@gmail.com>
2026-09-25 15:29:52 +0900

    [fix] 메시징 초기화 로직 재사용을 위한 제거

--- frontend/settingFCM.ts
+++ frontend/settingFCM.ts
@@ -1,6 +1,5 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-import { getMessaging } from 'firebase/messaging';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -15,4 +14,3 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
-export const messaging = getMessaging(app);
